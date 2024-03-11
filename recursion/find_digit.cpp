b5c23ac0733ec90f2e2ed377c0a7b1fdb773ae2a
himanshukumar76 <himanshukumar75655893@gmail.com>
2024-03-11 15:44:14 +0530

    new files added

---
+++ recursion/find_digit.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string a = "123";
+    for (int i = 0; i < a.length(); i++)
+    {
+        int number = a[i] - '0';
+        cout << number << endl;
+    }
+    cout << endl; 
+    return 0;
+}
